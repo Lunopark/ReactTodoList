6f4941848ad62a60b4c7f802ef4f80bfb2ea4e0e
Lunopark <rostypod@gmail.com>
2021-09-20 18:39:11 +0300

    create app

---
+++ src/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const loader = () => (
+<div style={{ display: 'flex', justifyContent: 'center', margin: '.5rem' }}>
+  <div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+</div>)
+export default loader
